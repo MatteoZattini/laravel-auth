5f78641281a10dd3793f93639f5490c012c41b97
FilippoBonanno <fbonanno15@gmail.com>
2024-07-09 17:12:33 +0200

    Index view

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container-fluid mt-4">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <h1>SEI NELL'INDEX</h1>
+            </div>
+        </div>
+    </div>
+    </div>
+@endsection
