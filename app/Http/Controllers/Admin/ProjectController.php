5df79355b1ba34f112f4415a937003abed77996b
MatteoZattini <matteozattini@hotmail.com>
2024-07-09 17:08:07 +0200

    Spostato ProjectController dentro la cartella admin, creata una view Create, creata una voce Create nella dashboard

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -1,6 +1,7 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
+use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 
@@ -19,7 +20,7 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.create');
     }
 
     /**
