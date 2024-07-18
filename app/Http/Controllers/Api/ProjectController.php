82933a2d666145030f2d59c1734c1ab956c5e841
MatteoZattini <matteozattini@hotmail.com>
2024-07-18 17:16:26 +0200

    api created

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index() {
+        return response()->json([
+    
+            'projects' => Project::with('type')->OrderByDesc('id')->paginate(6)
+    
+        ]);
+    }
+}
